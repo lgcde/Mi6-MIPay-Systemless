1ddd8e8f837ee426821a30433cffa926c88b065d
lgcde <34829026+lgcde@users.noreply.github.com>
2019-05-10 22:37:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ wget $MIUI_CN_DEV_ZIP || exit 1
 
 unzip $(basename $MIUI_CN_DEV_ZIP) system.transfer.list system.new.dat.br || exit 1
 rm $(basename $MIUI_CN_DEV_ZIP)
-apt install brotli p7zip-full -y
+apt install brotli p7zip-full zip -y
 brotli --decompress system.new.dat.br
 python3 sdat2img.py system.transfer.list system.new.dat || exit 1
-rm system.transfer.list system.new.dat
+rm system.transfer.list system.new.dat system.new.dat.br
 7z x -omagisk/system system.img lib64/libentryexpro.so lib64/libuptsmaddonmi.so app/Mipay app/NextPay app/TSMClient app/UPTsmService || exit 1
 rm system.img
 
